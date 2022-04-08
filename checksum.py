cc1f9bda6d5d846eb4648c742232a9c0250d4452
mrhappyasthma <mrhappyasthma@gmail.com>
2022-04-08 05:44:58 -0700

    Extract CRC-32 checksums from `.s2z` archives
    MD5 checksums on a zip file (i.e. `.s2z`) is unreliable due to changing metadata. Instead, iterate those zip archives and compare the pre-computed CRC-32 checksums.
    
    Verified the checksum works for official install of x86_64 for 4.9.5 as well as then updating it to 4.10.1.
    
    Fixes https://github.com/HoN-Revival/HoN-Install-Checksums/issues/1

--- checksum.py
+++ checksum.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 import os
+import zipfile
 
 HON_PATH='C:\Program Files\Heroes of Newerth x64'
 
@@ -10,33 +11,44 @@ EXCLUDED_FILE_MATCHERS = [
   'HoN_ModMan.exe',
   'manifest.xml',
   'GPUCache',
-  'shadercache',
   'Ionic.Zip.Reduced.dll',  # for mod manager
-  'debug.log'
+  'debug.log',
+  'changed.txt',
+  'shadercache.xml',
+  'shadercache2.xml'
 ]
 
 checksums = {}
 for root, dirs, files in os.walk(HON_PATH, topdown=True):
    for name in files:
       path = os.path.join(root, name)
-
-      skip = False
-      for excluded_file_matcher in EXCLUDED_FILE_MATCHERS:
-        if excluded_file_matcher in path:
-          print('Skipping: ' + name)
-          skip = True
-      if skip:
-        continue
-
       relative_path = os.path.join(os.path.relpath(root, HON_PATH), name)
       if relative_path.startswith('.\\'):
         relative_path = relative_path.replace('.\\', '')
-      with open(path, 'rb') as file:
-        data = file.read()
-        checksum = hashlib.md5(data).hexdigest()
-        checksums[relative_path] = checksum
+
+      skip = False
+      for excluded_file_matcher in EXCLUDED_FILE_MATCHERS:
+        if excluded_file_matcher in relative_path:
+          print('Skipping: ' + relative_path)
+          skip = True
+      if skip:
+        continue
+        
+      # NOTE: `.s2z` files are just `.zip` files. Computing a checksum on the
+      # compressed file is unreliable. Instead, iterate through the archive
+      # and extract the pre-computed CRC-32 values.
+      if '.s2z' in name:
+        z=zipfile.ZipFile(path,'r')
+        for info in z.infolist():
+          filename = info.filename.replace('/', '\\')
+          checksums[os.path.join(relative_path, filename)] = hex(info.CRC)
+      else:  # Compute a MD5
+        with open(path, 'rb') as file:
+          data = file.read()
+          checksum = hashlib.md5(data).hexdigest()
+          checksums[relative_path] = checksum
 
 outfile_path = 'hon_checksum_dump.json'
 with open(outfile_path, 'w') as outfile:
   print('Dumping checksum dict as json to: ' + outfile_path)
-  json.dump(checksums, outfile)
+  json.dump(checksums, outfile, indent=4)
--- verify_checksum.py
+++ verify_checksum.py
@@ -1,8 +1,9 @@
 import hashlib
 import json
 import os
+import zipfile
 
-HON_INSTALL_PATH='L:\Program Files\Heroes of Newerth x64'
+HON_INSTALL_PATH='C:\Program Files\Heroes of Newerth x64'
 # TODO: Take in the architecture, version, and os as input arguments
 HON_CHECKSUM_REL_PATH='checksum dumps\\wac\\x86_64\\4.10.1.json'
 
@@ -12,10 +13,19 @@ EXCLUDED_FILE_MATCHERS = [
   'HoN_ModMan.exe',
   'manifest.xml',
   'GPUCache',
-  'shadercache',
   'Ionic.Zip.Reduced.dll',  # for mod manager
-  'debug.log'
+  'debug.log',
+  'changed.txt',
 ]
+
+def validate_checksum(checksum, expected_checksum, relative_path):
+  if not expected_checksum:
+    print(f'Extra file found: {relative_path} : {checksum}')
+    return False
+  if checksum != expected_checksum:
+    print(f'Checksum mismatch! File: {relative_path}\nExpected checksum: {expected_checksum}\nActual checksum: {checksum}')
+    return False
+  return True
 
 checksums = None
 with open(HON_CHECKSUM_REL_PATH, 'r') as checksum_file:
@@ -30,30 +40,41 @@ checksum_failed = False
 for root, dirs, files in os.walk(HON_INSTALL_PATH, topdown=True):
    for name in files:
       path = os.path.join(root, name)
+      relative_path = os.path.join(os.path.relpath(root, HON_INSTALL_PATH), name)
+      if relative_path.startswith('.\\'):
+        relative_path = relative_path.replace('.\\', '')
 
       skip = False
       for excluded_file_matcher in EXCLUDED_FILE_MATCHERS:
-        if excluded_file_matcher in path:
-          print('Skipping: ' + name)
+        if excluded_file_matcher in relative_path:
+          print('Skipping: ' + relative_path)
           skip = True
       if skip:
         continue
 
-      relative_path = os.path.join(os.path.relpath(root, HON_INSTALL_PATH), name)
-      if relative_path.startswith('.\\'):
-        relative_path = relative_path.replace('.\\', '')
-      with open(path, 'rb') as file:
-        data = file.read()
-        checksum = hashlib.md5(data).hexdigest()
-        expected_checksum = checksums.get(relative_path, '')
-        if not expected_checksum:
-          print(f'Extra file found: {relative_path} : {checksum}')
-          checksum_failed = True
-          continue
-        if checksum != expected_checksum:
-          print(f'Checksum mismatch! File: {relative_path}\nExpected checksum: {expected_checksum}\nActual checksum: {checksum}')
-          checksum_failed = True
-          continue
+        
+      # NOTE: `.s2z` files are just `.zip` files. Computing a checksum on the
+      # compressed file is unreliable. Instead, iterate through the archive
+      # and extract the pre-computed CRC-32 values.
+      if '.s2z' in name:
+        z = zipfile.ZipFile(path,'r')
+        for info in z.infolist():
+          checksum = hex(info.CRC)
+          filename = info.filename.replace('/', '\\')
+          zip_relative_path = os.path.join(relative_path, filename)
+          expected_checksum = checksums.get(zip_relative_path, '')
+          success = validate_checksum(checksum, expected_checksum, zip_relative_path)
+          if not success:
+            checksum_failed = True
+      else:  # verify MD5
+        with open(path, 'rb') as file:
+          data = file.read()
+          checksum = hashlib.md5(data).hexdigest()
+          expected_checksum = checksums.get(relative_path, '')
+          success = validate_checksum(checksum, expected_checksum, relative_path)
+          if not success:
+            checksum_failed = True
+
 
 if checksum_failed:
   print('\nChecksum failed :(')
