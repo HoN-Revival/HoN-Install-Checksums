cd728236b5847f4abeb09149a7aad5445699aa6a
mrhappyasthma <mrhappyasthma@gmail.com>
2022-04-08 04:08:54 -0700

    Initial commit of checksum script
    From the fixed path to a HoN install, it computes a mapping of file->md5. This is then dumped as json to a local file.

---
+++ checksum.py
@@ -0,0 +1,39 @@
+import hashlib
+import json
+import os
+
+HON_PATH='C:\Program Files\Heroes of Newerth x64'
+
+EXCLUDED_FILE_MATCHERS = [
+  '.honmod',
+  'resources999.s2z',
+  'HoN_ModMan.exe',
+  'manifest.xml',
+  'GPUCache'
+]
+
+checksums = {}
+for root, dirs, files in os.walk(HON_PATH, topdown=True):
+   for name in files:
+      path = os.path.join(root, name)
+
+      skip = False
+      for excluded_file_matcher in EXCLUDED_FILE_MATCHERS:
+        if excluded_file_matcher in path:
+          print('Skipping: ' + name)
+          skip = True
+      if skip:
+        continue
+
+      relative_path = os.path.join(os.path.relpath(root, HON_PATH), name)
+      if relative_path.startswith('.\\'):
+        relative_path = relative_path.replace('.\\', '')
+      with open(path, 'rb') as file:
+        data = file.read()
+        checksum = hashlib.md5(data).hexdigest()
+        checksums[relative_path] = checksum
+
+outfile_path = 'hon_checksum_dump.json'
+with open(outfile_path, 'w') as outfile:
+  print('Dumping checksum dict as json to: ' + outfile_path)
+  json.dump(checksums, outfile)
